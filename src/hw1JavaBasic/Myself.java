2ad792616e310c272ec0bb40bf5a40607ecaf18a
hasinamomotaj <hasinaiubd@gmail.com>
2022-05-17 21:18:25 -0400

    touch

---
+++ src/hw1JavaBasic/Myself.java
@@ -0,0 +1,9 @@
+package hw1JavaBasic;
+//dmdnfdmfndm
+
+public    class Myself {
+public    static void main(String[] args) {
+	    System.out.println("Hasina Momotaj");
+	System.out.println("24 benz drive");
+}
+}
---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,11 @@
+package hw2JavaBasics;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Hasina");
+		System.out.print("I love to drive" + "\n My car info below :");
+		System.out.print("\n Nissan" + "\t Rouse" + "\n 2020" + "\t Blue" + "\t4wd");
+	}
+
+}
